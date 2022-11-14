55629e70ee06a1df149257e078a56b27401fa3b4
frankdonny <arkohfrank1@gmail.com>
2022-11-14 13:46:00 +0000

    0x15. API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
+"""gather data from api using this module"""
 import requests
 from sys import argv
 
-Base1 = "https://jsonplaceholder.typicode.com/todos/"
-Base2 = "https://jsonplaceholder.typicode.com/users/"
-response1 = requests.get(Base1).json()
-response2 = requests.get(Base2).json()
+def main():
+    base1 = "https://jsonplaceholder.typicode.com/todos/"
+    base2 = "https://jsonplaceholder.typicode.com/users/"
+    response1 = requests.get(base1).json()
+    response2 = requests.get(base2).json()
 
-ct = 0
-true_ = 0
-ls = []
-total = 0
-titles = ""
-for dict_ in response1:
-    for user in response2:
-        if user['id'] == int(argv[1]):
-            ls.append(user['name'])
-            break
-    if dict_['userId'] == int(argv[1]):
-        ct += 1
-        if dict_['completed'] == bool('True'):
-            true_ += 1
-            titles = titles + '\t' + dict_['title'] + '\n'
+    ct = 0
+    true_ = 0
+    ls = []
+    titles = ""
+    for dict_ in response1:
+        for user in response2:
+            if user['id'] == int(argv[1]):
+                ls.append(user['name'])
+                break
+        if dict_['userId'] == int(argv[1]):
+            ct += 1
+            if dict_['completed'] == bool('True'):
+                true_ += 1
+                titles = titles + '\t' + dict_['title'] + '\n'
 
-print("Employee {} is done with tasks({}/{}):\n{}".format( ls[0],true_, ct, titles))
-# print()
+    print("Employee {} is done with tasks({}/{}):\n{}".
+          format(ls[0], true_, ct, titles))
+
+if __name__ == "__main__":
+    main()
